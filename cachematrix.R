8a5c7d493e7c37409db703b655dc59b11b2d9648
amc94 <80308667+amc94@users.noreply.github.com>
2022-01-10 19:18:44 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makes a matrix cache object that caches its own inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mat_inverse <- NULL
+        
+        set <- function(y) {
+                x <- y
+                mat_inverse <- NULL
+        }
+        
+        get <- function() x
+        
+        set_inverse <- function(inv){
+                mat_inverse <<- inv
+        }
+        
+        get_inverse <- function() mat_inverse
+        
+        list(set=set, get=get,
+             set_inverse=set_inverse,
+             get_inverse=get_inverse)
 }
 
 
@@ -12,4 +26,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        mat_inverse <- x$get_inverse()
+        
+        if(!is.null(mat_inverse)){
+                message("getting cached data")
+                return(mat_inverse)
+        }
+        
+        mat <- x$get()
+        mat_inverse <- solve(x$get(), ...)
+        x$set_inverse(mat_inverse)
+        mat_inverse
 }
+
+##Tests
+mat <- matrix(c(1,2,3,4),2,2)
+mat_inverse <- solve(mat)
+
+cached_mat <- makeCacheMatrix(matrix(c(1,2,3,4),2,2))
+stopifnot(identical(mat, cached_mat$get()))
+
+cacheSolve(cached_mat)
+stopifnot(identical(mat_inverse,cached_mat$get_inverse()))
+
+cacheSolve(cached_mat)
